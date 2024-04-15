a3cecd22aa391dc3b3d44e72f63f5033c60a2dc0
jones58 <108466184+jones58@users.noreply.github.com>
2024-04-15 17:45:01 +0100

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,24 +9,7 @@ module.exports = {
       colors: {
         "accent-1": "#ddd4c6",
         "accent-2": "#EAEAEA",
-        "accent-7": "#333",
-        success: "#0070f3",
-        cyan: "#79FFE1",
-      },
-      spacing: {
-        28: "7rem",
-      },
-      letterSpacing: {
-        tighter: "-.04em",
-      },
-      lineHeight: {
-        tight: 1.2,
-      },
-      fontSize: {
-        "5xl": "2.5rem",
-        "6xl": "2.75rem",
-        "7xl": "4.5rem",
-        "8xl": "6.25rem",
+        "accent-3": "#333",
       },
       boxShadow: {
         small: "0 5px 10px rgba(0, 0, 0, 0.12)",
