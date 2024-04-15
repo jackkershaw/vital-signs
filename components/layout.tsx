0fb27575245a68c7f545671c06f174e7acd83665
jones58 <108466184+jones58@users.noreply.github.com>
2024-04-15 16:11:34 +0100

    remove container tsx file

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,14 +1,16 @@
+import Header from "./header";
 import Footer from "./footer";
 import Meta from "./meta";
 
 export default function Layout({ preview, children }) {
   return (
-    <>
+    <div className="container mx-auto px-5">
+      <Header />
       <Meta />
       <div className="min-h-screen">
         <main>{children}</main>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
