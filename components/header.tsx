bf5921861ffb84af17977490e4b8da17ec3cf299
jones58 <108466184+jones58@users.noreply.github.com>
2024-04-19 08:22:57 +0100

    edit header styling

--- components/header.tsx
+++ components/header.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div>
-      <div className="mx-auto container px-5 flex flex-row justify-between my-5 space-x-10 text-3xl">
+    <div className="bg-orange-50">
+      <div className="mx-auto container px-5 py-2 flex flex-row justify-between space-x-10 text-3xl bg-orange-50">
         <Image
           src="/images/banner-draft.png"
           alt="logo"
           width={300}
           height={200}
         ></Image>
-        <div className="flex flex-row space-x-10 h-20 items-center justify-center my-10">
+        <div className="flex flex-row space-x-10 h-20 items-center justify-center mt-10">
           <Link href="/about" className="bg-accent-1 p-4">
             ABOUT
           </Link>
@@ -21,7 +21,11 @@ export default function Header() {
           <Link href="/contact" className="bg-accent-1 p-4">
             CONTACT
           </Link>
-          <div className="bg-accent-1 p-4">F, T, I links</div>
+          <div className="bg-accent-1 p-4">
+            <span className="icon-[uil--facebook]"></span>
+            <span className="icon-[bi--twitter-x]"></span>
+            <span className="icon-[mdi--instagram]"></span>
+          </div>
         </div>
       </div>
     </div>
