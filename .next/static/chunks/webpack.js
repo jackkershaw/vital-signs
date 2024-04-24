45986a54015cfdea3e516ffc7e403af944a77bd5
jones58 <108466184+jones58@users.noreply.github.com>
2024-04-24 11:53:39 +0100

    update favicon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e7de9239aca58d16"; }
+/******/ 		__webpack_require__.h = function() { return "816f66f59bd3a196"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
