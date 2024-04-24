cbc573163ce12720a73496bea706277263c4ad59
jones58 <108466184+jones58@users.noreply.github.com>
2024-04-24 16:32:14 +0100

    nextjs build again

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3ffb2b8de6042b14"; }
+/******/ 		__webpack_require__.h = function() { return "c10baf3e4b5eefdb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
