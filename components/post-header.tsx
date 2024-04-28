6c4d0f45a974c7d6184ba0a6c4ac67f22c39484d
jones58 <108466184+jones58@users.noreply.github.com>
2024-04-28 17:04:47 +0100

    spacing edit

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -3,7 +3,7 @@ import PostTitle from "./post-title";
 
 export default function PostHeader({ title, coverImage }) {
   return (
-    <div className="sm:flex sm:flex-row sm:justify-center">
+    <div className="sm:flex sm:flex-row sm:justify-center sm:pb-8">
       <div className="flex flex-col sm:items-end sm:flex-row sm:gap-x-10">
         <div className="">
           <CoverImage title={title} coverImage={coverImage} />
