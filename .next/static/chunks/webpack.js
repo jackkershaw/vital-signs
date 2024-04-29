73ea61a7eaccd62bbad7e20d7f5028a03967692b
jones58 <108466184+jones58@users.noreply.github.com>
2024-04-29 09:00:11 +0100

    add aria labels again

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "512d6883e2327306"; }
+/******/ 		__webpack_require__.h = function() { return "79676e1bcc51493d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
