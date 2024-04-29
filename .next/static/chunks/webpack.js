a5b15d84826dc61c16971bd65d63aa3aba974d5f
jones58 <108466184+jones58@users.noreply.github.com>
2024-04-29 09:35:31 +0100

    undo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2d8f8912f5ea7a0d"; }
+/******/ 		__webpack_require__.h = function() { return "263a689def418115"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/news.tsx
+++ pages/news.tsx
@@ -1,13 +1,39 @@
 import Layout from "../components/layout";
+import { getAllPostsForHome } from "../lib/api";
+import PostPreview from "../components/post-preview";
+import { GetStaticProps } from "next";
 
-export default function About() {
+export default function About({ allPosts }) {
   return (
     <div>
       <Layout>
         <div className="grid grid-cols-2 gap-10 py-10">
-          This is the news categories page.
+          This is the news categories page. All categories listed and
+          selectable at top
+        </div>
+        <div className="sm:grid sm:grid-cols-2 sm:gap-x-5">
+          {allPosts.map(({ node }) => (
+            <PostPreview
+              key={node.slug}
+              title={node.title}
+              coverImage={node.featuredImage}
+              slug={node.slug}
+              excerpt={node.excerpt}
+              category={node.categories.nodes.map(
+                (category) => category.name
+              )}
+            />
+          ))}
         </div>
       </Layout>
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async ({}) => {
+  const allPosts = await getAllPostsForHome();
+  return {
+    props: { allPosts },
+    revalidate: 10,
+  };
+};
