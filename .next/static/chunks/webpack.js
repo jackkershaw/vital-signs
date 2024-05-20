541990536ec33407e2d5d95ad17c13020b13e181
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-20 12:14:43 +0100

    remove cors support

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d553151a0bf43815"; }
+/******/ 		__webpack_require__.h = function() { return "e803936868de9945"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
