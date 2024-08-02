a243c69db0c5290aae40d6c6a06546dfe81c5cd1
jones58 <108466184+jones58@users.noreply.github.com>
2024-08-02 15:17:28 +0100

    add lazy motion animations

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,7 +2,7 @@ import Header from "./header";
 import Footer from "./footer";
 import Meta from "./meta";
 import Head from "next/head";
-import { motion } from "framer-motion";
+import { m, LazyMotion, domAnimation } from "framer-motion";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
@@ -20,31 +20,33 @@ export default function Layout({ children }) {
       <SpeedInsights />
       <Meta />
       <Header />
-      <motion.div
-        initial="pageInitial"
-        animate="pageAnimate"
-        variants={{
-          pageInitial: {
-            y: 20,
-            opacity: 0,
-            scale: 0.99,
-          },
-          pageAnimate: {
-            y: 0,
-            opacity: 1,
-            scale: 1,
-            transition: {
-              duration: 0.4,
-              ease: "easeInOut",
-              when: "beforeChildren",
+      <LazyMotion features={domAnimation}>
+        <m.div
+          initial="pageInitial"
+          animate="pageAnimate"
+          variants={{
+            pageInitial: {
+              y: 20,
+              opacity: 0,
+              scale: 0.99,
             },
-          },
-        }}
-      >
-        <main className="bg-neutral-50 text-customRed-950 px-5 lg:px-10 lg:mx-10 min-h-[60vh] lg:mt-10 lg:py-5">
-          {children}
-        </main>
-      </motion.div>
+            pageAnimate: {
+              y: 0,
+              opacity: 1,
+              scale: 1,
+              transition: {
+                duration: 0.4,
+                ease: "easeInOut",
+                when: "beforeChildren",
+              },
+            },
+          }}
+        >
+          <main className="bg-neutral-50 text-customRed-950 px-5 lg:px-10 lg:mx-10 min-h-[60vh] lg:mt-10 lg:py-5">
+            {children}
+          </main>
+        </m.div>
+      </LazyMotion>
       <Footer />
     </div>
   );
