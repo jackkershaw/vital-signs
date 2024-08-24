1db9edc8ef6c928a2f6ffb1473279936500bf3c5
Jack <jackkershaw@protonmail.com>
2024-08-24 17:45:16 +0100

    Update umami tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-<script defer src="https://umami.jackkershaw.pp.ua/script.js" data-website-id="945ea039-f928-49af-8015-93713ba100ca"></script>
       <script
-        defer
-        src="https://cloud.umami.is/script.js"
-        data-website-id="54de3e95-17b4-4d5f-889c-551763432a69"
+        defer        src="https://cloud.umami.is/script.js"        data-website-id="54de3e95-17b4-4d5f-889c-551763432a69"
       ></script>
+<script defer src="https://umami.jackkershaw.pp.ua/script.js" data-website-id="945ea039-f928-49af-8015-93713ba100ca"></script>
       <body>
         <Main />
         <NextScript />
