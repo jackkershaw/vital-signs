92ca542794fdea9b2712952a2f247ce4de70589f
jackkershaw <hello@jackkershaw.net>
2025-04-28 21:18:35 +0100

    turn off image optimisations

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ const { protocol, hostname, port, pathname } = new URL(
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
